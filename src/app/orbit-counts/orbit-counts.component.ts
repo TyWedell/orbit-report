484b2452ec0ef08e11088b8a510c6fb0b7136ecc
TyWedell <tywedell@gmail.com>
2020-09-19 07:15:26 -0500

    created orbit-counts component

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
